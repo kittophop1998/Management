25893226f8fb97afbf960658c1cfea39361420a1
kittiphop <kittiphopsompuech2@gmail.com>
2021-04-21 22:03:51 +0700

    commit1

---
+++ resources/views/vender.blade.php
@@ -0,0 +1,52 @@
+<html lang="en">
+  <title>ระบบจัดการภายใน</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="/">Accellum Technology</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li><a href="/">Home</a></li>
+      <li><a href="/leave">บันทึกการลา</a></li>
+      <li class="active"><a href="/keepvender">เก็บรูป สินค้า</a></li>
+      <!-- <li><a href="#">Page 3</a></li> -->
+    </ul>
+  </div>
+</nav>
+
+<!-- <style>
+  table {
+    padding-left: 50px;
+  }
+</style> -->
+
+
+<div class="container">
+  <h3>เก็บรูปสินค้า</h3>
+  <p>An inverted navbar is black instead of gray.</p>
+</div>
+
+<form method="post" action=''>
+  <table border='0'style="padding-left:50px">
+    <tr>
+      <td>ชื่อโครงงาน:</td>
+      <td><input type='text' name='name_project'></td>
+    </tr>
+    <tr>
+        <td> </td>
+    </tr>
+    <tr>
+      <td>หมายเลข serial:</td>
+      <td><input name='number'></td>
+    </tr>
+  </table>
+
+
+
+</form>
